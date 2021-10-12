eddb0452873808aa44e8a97f41151620228dd578
Nelson Herrera Castro <nelsonehc@gmail.com>
2021-10-12 03:19:10 -0500

    Cnectamos la BD a MongoDb Atlas

--- src/database/datab.js
+++ src/database/datab.js
@@ -1,3 +1,4 @@
 module.exports = {
-    datab: "mongodb://localhost:27017/vacunaproject"
+    //datab: "mongodb://localhost:27017/vacunaproject"
+    datab: "mongodb+srv://nelheca:juli280514@cluster0.za5mw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 }
